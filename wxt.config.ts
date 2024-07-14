7c95bffc14c727874479fbeabf6c5226027e692c
Justin Chung <20733699+justin13888@users.noreply.github.com>
2024-07-14 01:35:18 -0400

    Update manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    permissions: ['storage'],
+    permissions: [
+      'contextMenus',
+      'storage',
+      'tabs',
+    ],
     // browser_action
     // browser_specific_settings 
     author: "Justin Chung"
