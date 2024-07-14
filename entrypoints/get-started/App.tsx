1d2c1a2b097a4a184b9e553b439f2563227d6647
Justin Chung <20733699+justin13888@users.noreply.github.com>
2024-07-14 03:08:39 -0400

    Add get-started

---
+++ entrypoints/get-started/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+// TODO: Implement
+function App() {
+  return (
+    <div class="min-h-screen flex items-center justify-center bg-gray-200">
+      <p>Get started</p>
+    </div>
+  );
+
+}
+
+export default App;
