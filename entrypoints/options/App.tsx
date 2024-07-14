df1bc937d7b18f7026d68430dc4b6b3362a96da5
Justin Chung <20733699+justin13888@users.noreply.github.com>
2024-07-14 03:09:45 -0400

    Add options

---
+++ entrypoints/options/App.tsx
@@ -0,0 +1,17 @@
+import { createSignal } from 'solid-js';
+import solidLogo from '@/assets/solid.svg';
+import wxtLogo from '/wxt.svg';
+import './App.css';
+
+// TODO: Implement
+function App() {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default App;
