bebd56054a5aed73e6d1aa745c0f5eb5e27db066
Justin Chung <20733699+justin13888@users.noreply.github.com>
2024-07-15 15:12:59 -0400

    Add 404 redirect

--- entrypoints/index/App.tsx
+++ entrypoints/index/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Route } from "@solidjs/router"
+import { HashRouter, Navigate, Route } from "@solidjs/router"
 import { MetaProvider, Title } from "@solidjs/meta";
 
 export const App = () => {
@@ -8,6 +8,7 @@ export const App = () => {
             <HashRouter>
                 <Route path="/saved" component={lazy(() => import("./routes/saved"))} />
                 <Route path="/import" component={lazy(() => import("./routes/import"))} />
+                <Route path="*" component={() => <Navigate href="/saved" />} />
             </HashRouter>
         </MetaProvider>
     )
