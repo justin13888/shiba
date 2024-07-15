dbd6399c890e9660df5b909a285d1966f5e4bad2
Justin Chung <20733699+justin13888@users.noreply.github.com>
2024-07-15 15:48:25 -0400

    Extract header

--- entrypoints/index/App.tsx
+++ entrypoints/index/App.tsx
@@ -1,10 +1,12 @@
 import { HashRouter, Navigate, Route } from "@solidjs/router"
 import { MetaProvider, Title } from "@solidjs/meta";
+import { Header } from "@/components/header";
 
 export const App = () => {
     return (
         <MetaProvider>
             <Title>Shiba</Title>
+            <Header />
             <HashRouter>
                 <Route path="/saved" component={lazy(() => import("./routes/saved"))} />
                 <Route path="/import" component={lazy(() => import("./routes/import"))} />
