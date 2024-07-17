c67143818f69e560b89e74be5c6e1698aa1452f8
Justin Chung <20733699+justin13888@users.noreply.github.com>
2024-07-17 01:52:26 -0400

    Correct titles

--- shiba/wxt.config.ts
+++ shiba/wxt.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
     manifest: {
+        name: "Shiba",
         permissions: ["contextMenus", "storage", "tabs"],
         // browser_action
         // browser_specific_settings
