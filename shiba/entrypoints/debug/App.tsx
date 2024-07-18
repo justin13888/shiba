7484fc74e048ea5865dad571764a93b1aa2de441
Justin Chung <20733699+justin13888@users.noreply.github.com>
2024-07-17 21:30:45 -0400

    Implement centralized, async logging

--- shiba/entrypoints/debug/App.tsx
+++ shiba/entrypoints/debug/App.tsx
@@ -1,9 +1,132 @@
+import { getLogs, Logger, LogLevel } from "@/utils/logger";
+import type { Component } from "solid-js";
+
 // TODO: Implement
 // TODO: View database
-// TODO: View logs
+const App: Component = () => {
+    const logger = new Logger(import.meta.url);
+    const addTestLogs = async () => {
+        console.log("Adding test logs");
+        logger.info("Test: This is an informational message", {
+            additional: "data",
+        });
+        logger.error("Test: This is an error message", { errorCode: 123 });
+    };
 
-function App() {
-    return <p>hello</p>;
+    const [page, setPage] = createSignal(1);
+    const [pageLimit, setPageLimit] = createSignal(10);
+    const [lastKeys, setLastKeys] = createSignal<number[]>([]);
+
+    createEffect(() => {
+
+        console.log("Last keys:", lastKeys());
+    }   );
+
+    const getLastKey = () => (page() > 1 ? lastKeys()[page() - 2] : undefined);
+    // Resource to fetch logs based on the current page
+    const [logs, { mutate }] = createResource(
+        () => [getLastKey(), pageLimit()] as [number | undefined, number],
+        async ([lastKey, pageLimit]) => {
+      
+      const result = await getLogs(lastKey, pageLimit);
+      
+      // Update lastKeys array with the last key of the fetched logs
+      if (result.length > 0) {
+        setLastKeys((prev) => {
+          const newKeys = [...prev];
+          newKeys[page() - 1] = result[result.length - 1].id;
+          return newKeys;
+        });
+      }
+      return result;
+    });
+    // TODO: Test behaviour when page, pageLimit are changed
+    // TODO: Test behaviour after refetch()
+  
+    const handleNextPage = () => {
+      if (logs()?.length === pageLimit()) {
+        setPage(page() + 1);
+      }
+    };
+  
+    const handlePreviousPage = () => {
+      if (page() > 1) {
+        setPage(page() - 1);
+      }
+    };
+
+    return (
+        <div class="container">
+            <div class="pb-4 align-middle">
+                <h1 class="text-4xl font-extrabold">Shiba Debug</h1>
+            </div>
+            <button type="button" onClick={addTestLogs}>
+                Add logs
+            </button>
+            <div class="flex flex-col">
+                <Show when={logs.state === "ready"} fallback={<p>Loading logs...</p>}>
+                    <ul class="list-none h-[200px] overflow-y-auto">
+                        <For each={logs()}>
+                            {({
+                                id, // TODO: Fix type because id is implicitly added
+                                timestamp,
+                                level,
+                                identifier,
+                                message,
+                                meta,
+                            }) => (
+                                <li class="flex flex-row">
+                                    <div class="flex flex-col">
+                                        <span class="p-1">
+                                            [{id}]
+                                            &nbsp;
+                                            [{
+                                                new Date(timestamp).toISOString()
+                                            }]
+                                            &nbsp;
+                                            [{identifier || "UNKNOWN"}]
+                                            &nbsp;
+                                            <Switch fallback={<span>UNKNOWN</span>}>
+                                                <Match when={level === LogLevel.DEBUG}>
+                                                    <span class="text-gray-500">DEBUG</span>
+                                                </Match>
+                                                <Match when={level === LogLevel.INFO}>
+                                                    <span class="text-blue-500">INFO</span>
+                                                </Match>
+                                                <Match when={level === LogLevel.WARN}>
+                                                    <span class="text-yellow-500">WARN</span>
+                                                </Match>
+                                                <Match when={level === LogLevel.ERROR || level === LogLevel.FATAL}>
+                                                    <span class="text-red-500">ERROR</span>
+                                                </Match>
+                                            </Switch>
+                                            &nbsp;
+                                            <span>{message}</span>
+                                        </span>
+                                        <pre class="ml-4">{JSON.stringify(meta, null, 2)}</pre>
+                                    </div>
+                                </li>
+                            )}
+                        </For>
+                    </ul>
+                </Show>
+            </div>
+            <div class="flex flex-row space-x-6">
+            <button type="button" onClick={handlePreviousPage} disabled={page() === 1}>
+                Previous Page
+            </button>
+            <button type="button" onClick={handleNextPage} disabled={(() => {
+                const l = logs();
+                if (l === undefined) return true;
+
+                return (l.length || 0) < pageLimit();
+            })()}>
+                Next Page
+            </button>
+            </div>
+            <p>Page count: {page()}</p>
+        </div>
+    );
 }
 
 export default App;
---
+++ shiba/utils/logger.ts
@@ -0,0 +1,180 @@
+
+import { openDB, type DBSchema } from 'idb';
+
+/** Log Level */
+export enum LogLevel {
+    TRACE = "TRACE",
+    DEBUG = "DEBUG",
+    INFO = "INFO",
+    WARN = "WARN",
+    ERROR = "ERROR",
+    FATAL = "FATAL",
+};
+
+/** Map log levels to numeric values */
+const LEVEL_MAP: Record<LogLevel, number> = {
+    [LogLevel.TRACE]: 10,
+    [LogLevel.DEBUG]: 20,
+    [LogLevel.INFO]: 30,
+    [LogLevel.WARN]: 40,
+    [LogLevel.ERROR]: 50,
+    [LogLevel.FATAL]: 60,
+};
+
+/** Level Filter */
+export enum LevelFilter {
+  ALL = "ALL",
+  TRACE = "TRACE",
+  DEBUG = "DEBUG",
+  INFO = "INFO",
+  WARN = "WARN",
+  ERROR = "ERROR",
+  FATAL = "FATAL",
+  OFF = "OFF",
+}
+
+const LEVEL_FILTER_MAP: Record<LevelFilter, number> = {
+  [LevelFilter.ALL]: 0,
+  [LevelFilter.TRACE]: 10,
+  [LevelFilter.DEBUG]: 20,
+  [LevelFilter.INFO]: 30,
+  [LevelFilter.WARN]: 40,
+  [LevelFilter.ERROR]: 50,
+  [LevelFilter.FATAL]: 60,
+  [LevelFilter.OFF]: 100,
+};
+
+export interface LogEntry {
+  /** Unix timstamp in milliseconds */
+  timestamp: number;
+  /**
+   * Log level
+   */
+  level: LogLevel;
+  /**
+   * Unique identifier for the logger.
+   */
+  identifier: string;
+  message: string;
+  // biome-ignore lint/suspicious/noExplicitAny: Required
+  meta: any[];
+};
+
+// Define your IndexedDB schema
+interface LogDB extends DBSchema {
+  logs: {
+    key: string;
+    value: LogEntry;
+  };
+}
+
+// Initialize IndexedDB
+const dbPromise = openDB<LogDB>('logs', 1, {
+    upgrade(db) {
+      db.createObjectStore('logs', { keyPath: 'id', autoIncrement: true });
+    },
+  });
+
+/**
+ * Get logs from IndexedDB from newest to oldest.
+ * @param cursor Cursor to start from
+ * @param limit Maximum number of logs to retrieve
+ */
+export const getLogs = async (lastKey: number | undefined, limit: number) => {
+    const db = await dbPromise;
+    const tx = db.transaction('logs', 'readonly');
+    const store = tx.objectStore('logs');
+
+    const logs: LogEntry[] = [];
+
+    let cursor = await store.openCursor(
+        lastKey ? IDBKeyRange.upperBound(lastKey, true) : undefined,
+        'prev',
+    );
+
+    while (cursor && logs.length < limit) {
+        logs.push(cursor.value);
+        cursor = await cursor.continue();
+    }
+
+    await tx.done;
+
+    return logs;
+};
+
+const DEFAULT_LOG_LEVEL: LevelFilter = LevelFilter.DEBUG; // TODO: Make this configurable and defined via extension settings
+
+export class Logger {
+  /**
+   * Unique identifier for the logger.
+   */
+  identifier: string;
+
+  /**
+   * Minimum log level to log.
+   */
+  level: number;
+  // TODO: attribute
+
+  /**
+   * 
+   * @param identifier Unique identifier for the logger.
+   */
+  constructor(identifier: string) {
+    this.identifier = identifier;
+    this.level = LEVEL_FILTER_MAP[DEFAULT_LOG_LEVEL];
+  }
+
+  /**
+   * Logs a message
+   * @param level Log level
+   */
+  // biome-ignore lint/suspicious/noExplicitAny: Required
+  private _log(level: LogLevel, message: string, meta: any[]) {
+    if (!this._shouldLog(level)) {
+      return;
+    }
+
+    const logEntry: LogEntry = {
+      timestamp: Date.now(),
+      level,
+      identifier: this.identifier,
+      message,
+      meta: structuredClone(meta),
+    };
+
+    // Save to IndexedDB
+    dbPromise.then(async (db) => {
+      await db.add('logs', logEntry);
+    });
+  }
+
+  private _shouldLog(level: LogLevel): boolean {
+    return LEVEL_MAP[level] >= this.level;
+  }
+
+  // biome-ignore lint/suspicious/noExplicitAny: Require
+  trace(message: string, ...meta: any[]) { this._log(LogLevel.TRACE, message, meta); }
+  // biome-ignore lint/suspicious/noExplicitAny: Require
+  debug(message: string, ...meta: any[]) { this._log(LogLevel.DEBUG, message, meta); }
+  // biome-ignore lint/suspicious/noExplicitAny: Require
+  info(message: string, ...meta: any[]) { this._log(LogLevel.INFO, message, meta); }
+  // biome-ignore lint/suspicious/noExplicitAny: Require
+  warn(message: string, ...meta: any[]) { this._log(LogLevel.WARN, message, meta); }
+  // biome-ignore lint/suspicious/noExplicitAny: Require
+  error(message: string, ...meta: any[]) { this._log(LogLevel.ERROR, message, meta); }
+  // biome-ignore lint/suspicious/noExplicitAny: Require
+  fatal(message: string, ...meta: any[]) { this._log(LogLevel.FATAL, message, meta); }
+}
+
+
+
+// export con
+
+// // TODO
+// // Initialize
+// // Log levels
+// // asynchronous logging + batching
+// // Saving to IndexedDB
+// // Formatter for pretty printing
+// // Log rollover
