611c2b88aec4f34642f0fb79ddfa79da9dbea1a1
Justin Chung <20733699+justin13888@users.noreply.github.com>
2024-07-21 19:41:56 -0400

    Update popup

--- shiba/entrypoints/popup/App.tsx
+++ shiba/entrypoints/popup/App.tsx
@@ -2,7 +2,6 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { Tab, TabGroup } from "@/types/model";
 import { addTabBundle, clearTabs } from "@/utils/db";
 import { saveAllTabs, saveCurrentTab } from "@/utils/tabs";
-import "./App.css";
 import { switchToOrOpenTab } from "@/utils";
 import { URLS } from "@/utils/constants";
 import { tabCount, tabDBRefetch } from "@/utils/store";
@@ -10,124 +9,128 @@ import { tabCount, tabDBRefetch } from "@/utils/store";
 // TODO: Style
 function App() {
     return (
-        <>
-            <div class="w-[300px] h-[300px] flex flex-col items-center justify-between p-1 bg-gray-100">
-                <div class="flex flex-col grow text-center items-center p-4">
-                    <Show
-                        when={
-                            tabCount.state === "ready" ||
-                            tabCount.state === "refreshing"
-                        }
-                        fallback={
-                            <Skeleton height={72} width={62} radius={10} />
-                        }
-                    >
-                        <p class="text-7xl font-extrabold text-gray-800">
-                            {tabCount()}
-                        </p>
-                    </Show>
-                    <p>Tabs Saved</p>
-                </div>
-
-                {/* Save Buttons */}
-                <div class="grid grid-cols-2 gap-1">
-                    <For
-                        each={
-                            [
-                                [
-                                    "Save Current Tab",
-                                    async () => {
-                                        const savedTabId =
-                                            await saveCurrentTab();
-                                        tabDBRefetch();
-                                        await switchToOrOpenTab(URLS.SAVED);
-                                        if (savedTabId !== undefined) {
-                                            browser.tabs.remove(savedTabId);
-                                        }
-                                        // TODO: Force refresh all saved pages (since the DB has been replaced)
-                                    },
-                                ],
-                                [
-                                    "Save All Tabs",
-                                    async () => {
-                                        const savedTabIds = await saveAllTabs();
-                                        tabDBRefetch();
-                                        await switchToOrOpenTab(URLS.SAVED);
-                                        for (const tabId of savedTabIds) {
-                                            browser.tabs.remove(tabId);
-                                        }
-                                        // TODO: Force refresh all saved pages (since the DB has been replaced)
-                                    },
-                                ],
-                                [
-                                    "Open Debug",
-                                    () => {
-                                        switchToOrOpenTab(URLS.DEBUG);
-                                    },
-                                ],
-                                [
-                                    "Open Saved",
-                                    () => {
-                                        switchToOrOpenTab(URLS.SAVED);
-                                    },
-                                ],
-                                [
-                                    "Open Options",
-                                    () => {
-                                        switchToOrOpenTab(URLS.OPTIONS);
-                                    },
-                                ],
-                                [
-                                    "Seed",
-                                    async () => {
-                                        // TODO: Remove this later after testing
-                                        const newTabGroup = new TabGroup();
-                                        const newTabs = Array.from(
-                                            { length: 10 },
-                                            () => {
-                                                const tab = new Tab({
-                                                    title: "Test Tab",
-                                                    url: "https://example.com",
-                                                    tabGroupId:
-                                                        newTabGroup.groupId,
-                                                });
-                                                tab.tabGroupId =
-                                                    newTabGroup.groupId;
-                                                return tab;
-                                            },
-                                        );
-
-                                        addTabBundle([newTabGroup, newTabs]);
-
-                                        tabDBRefetch();
-                                        await switchToOrOpenTab(URLS.SAVED);
-                                    },
-                                ],
-                                [
-                                    "Clear All",
-                                    async () => {
-                                        await clearTabs();
-                                        tabDBRefetch();
-                                    },
-                                ],
-                            ] as [string, () => void | Promise<void>][]
-                        }
-                    >
-                        {([text, handler]) => (
-                            <button
-                                onClick={handler}
-                                type="button"
-                                class="p-4 bg-blue-500 text-white text-sm font-semibold rounded-lg shadow-md hover:bg-blue-600 transition-colors"
-                            >
-                                {text}
-                            </button>
-                        )}
-                    </For>
-
-                    {/* TODO: Remove extra buttons after major changes */}
-                </div>
+        <div class="w-[300px] h-[300px] flex flex-col items-center justify-between p-1 bg-gray-100 rounded-sm">
+            <div class="flex flex-col grow text-center items-center p-4">
+                <Show
+                    when={
+                        tabCount.state === "ready" ||
+                        tabCount.state === "refreshing"
+                    }
+                    fallback={
+                        <Skeleton height={72} width={62} radius={10} />
+                    }
+                >
+                    <p class="text-7xl font-extrabold text-gray-800">
+                        {tabCount()}
+                    </p>
+                </Show>
+                <p>Tabs Saved</p>
             </div>
-        </>
+
+            {/* Save Buttons */}
+            <div class="grid grid-cols-2 gap-1">
+                <For
+                    each={
+                        [
+                            [
+                                "Save Current Tab",
+                                async () => {
+                                    const savedTabId =
+                                        await saveCurrentTab();
+                                    tabDBRefetch();
+                                    await switchToOrOpenTab(URLS.SAVED);
+                                    if (savedTabId !== undefined) {
+                                        browser.tabs.remove(savedTabId);
+                                    }
+                                    // TODO: Force refresh all saved pages (since the DB has been replaced)
+                                },
+                            ],
+                            [
+                                "Save All Tabs",
+                                async () => {
+                                    const savedTabIds = await saveAllTabs();
+                                    tabDBRefetch();
+                                    await switchToOrOpenTab(URLS.SAVED);
+                                    for (const tabId of savedTabIds) {
+                                        browser.tabs.remove(tabId);
+                                    }
+                                    // TODO: Force refresh all saved pages (since the DB has been replaced)
+                                },
+                            ],
+                            [
+                                "Open Debug",
+                                () => {
+                                    switchToOrOpenTab(URLS.DEBUG);
+                                },
+                            ],
+                            [
+                                "Open Saved",
+                                () => {
+                                    switchToOrOpenTab(URLS.SAVED);
+                                },
+                            ],
+                            [
+                                "Open Options",
+                                () => {
+                                    switchToOrOpenTab(URLS.OPTIONS);
+                                },
+                            ],
+                            [
+                                "Open History",
+                                () => {
+                                    switchToOrOpenTab(URLS.HISTORY);
+                                },
+                            ],
+                            [
+                                "Seed",
+                                async () => {
+                                    // TODO: Remove this later after testing
+                                    const newTabGroup = new TabGroup();
+                                    const newTabs = Array.from(
+                                        { length: 10 },
+                                        () => {
+                                            const tab = new Tab({
+                                                title: "Test Tab",
+                                                url: "https://example.com",
+                                                tabGroupId:
+                                                    newTabGroup.groupId,
+                                            });
+                                            tab.tabGroupId =
+                                                newTabGroup.groupId;
+                                            return tab;
+                                        },
+                                    );
+
+                                    addTabBundle([newTabGroup, newTabs]);
+
+                                    tabDBRefetch();
+                                    await switchToOrOpenTab(URLS.SAVED);
+                                },
+                            ],
+                            [
+                                "Clear All",
+                                async () => {
+                                    await clearTabs();
+                                    tabDBRefetch();
+                                },
+                            ],
+                        ] as [string, () => void | Promise<void>][]
+                    }
+                >
+                    {([text, handler]) => (
+                        <button
+                            onClick={handler}
+                            type="button"
+                            class="p-4 bg-blue-500 text-white text-sm font-semibold rounded-lg shadow-md hover:bg-blue-600 transition-colors"
+                        >
+                            {text}
+                        </button>
+                    )}
+                </For>
+
+                {/* TODO: Remove extra buttons after major changes */}
+            </div>
+        </div>
     );
 }
 
--- shiba/utils/constants.ts
+++ shiba/utils/constants.ts
@@ -1,7 +1,9 @@
 export const URLS = {
-    SAVED: browser.runtime.getURL("/index.html#/saved"),
-    IMPORT: browser.runtime.getURL("/index.html#/import"),
+    ANALYTICS: browser.runtime.getURL("/index.html#/analytics"),
     EXPORT: browser.runtime.getURL("/index.html#/export"),
+    HISTORY: browser.runtime.getURL("/index.html#/history"),
+    IMPORT: browser.runtime.getURL("/index.html#/import"),
+    SAVED: browser.runtime.getURL("/index.html#/saved"),
 
     GETTING_STARTED: browser.runtime.getURL("/getting-started.html"),
     OPTIONS: browser.runtime.getURL("/options.html"),
