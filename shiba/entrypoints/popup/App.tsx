b91ca57f0d5681ed9bcec9018902c20732f130ee
Justin Chung <20733699+justin13888@users.noreply.github.com>
2024-10-24 03:56:47 -0700

    Fix popup bugs

--- shiba/entrypoints/popup/App.tsx
+++ shiba/entrypoints/popup/App.tsx
@@ -3,8 +3,9 @@ import { Tab, TabGroup } from "@/types/model";
 import { switchToOrOpenTab } from "@/utils";
 import { URLS } from "@/utils/constants";
 import { addTabBundle, clearTabs } from "@/utils/db";
+import { queryClient } from "@/utils/query";
 import { tabCount, tabDBRefetch } from "@/utils/store";
-import { saveCurrentWindow, saveCurrentTab } from "@/utils/tabs";
+import { saveCurrentTab, saveCurrentWindow } from "@/utils/tabs";
 
 // TODO: Style
 function App() {
@@ -34,23 +35,30 @@ function App() {
                                 "Save Current Tab",
                                 async () => {
                                     const savedTabId = await saveCurrentTab();
-                                    tabDBRefetch();
+                                    tabDBRefetch(); // TODO: Remove
                                     await switchToOrOpenTab(URLS.SAVED);
                                     if (savedTabId !== undefined) {
                                         browser.tabs.remove(savedTabId);
                                     }
+                                    queryClient.invalidateQueries({
+                                        queryKey: ["tabgroups"],
+                                    });
                                     // TODO: Force refresh all saved pages (since the DB has been replaced)
                                 },
                             ],
                             [
                                 "Save All Tabs",
                                 async () => {
-                                    const savedTabIds = await saveCurrentWindow();
-                                    tabDBRefetch();
+                                    const savedTabIds =
+                                        await saveCurrentWindow();
+                                    tabDBRefetch(); // TODO: Remove
                                     await switchToOrOpenTab(URLS.SAVED);
                                     for (const tabId of savedTabIds) {
                                         browser.tabs.remove(tabId);
                                     }
+                                    queryClient.invalidateQueries({
+                                        queryKey: ["tabgroups"],
+                                    });
                                     // TODO: Force refresh all saved pages (since the DB has been replaced)
                                 },
                             ],
@@ -83,60 +91,71 @@ function App() {
                                 async () => {
                                     const testUrls = Object.freeze([
                                         {
-                                            "favicon": "https://www.typescriptlang.org/favicon-32x32.png?v=8944a05a8b601855de116c8a56d3b3ae",
-                                            "title": "TypeScript: JavaScript With Syntax For Types.",
-                                            "url": "https://www.typescriptlang.org/"
+                                            favicon:
+                                                "https://www.typescriptlang.org/favicon-32x32.png?v=8944a05a8b601855de116c8a56d3b3ae",
+                                            title: "TypeScript: JavaScript With Syntax For Types.",
+                                            url: "https://www.typescriptlang.org/",
                                         },
                                         {
-                                            "favicon": "https://huggingface.co/favicon.ico",
-                                            "title": "sentence-transformers (Sentence Transformers)",
-                                            "url": "https://huggingface.co/sentence-transformers"
+                                            favicon:
+                                                "https://huggingface.co/favicon.ico",
+                                            title: "sentence-transformers (Sentence Transformers)",
+                                            url: "https://huggingface.co/sentence-transformers",
                                         },
                                         {
-                                            "favicon": "https://vitejs.dev/logo.svg",
-                                            "title": "Vite | Next Generation Frontend Tooling",
-                                            "url": "https://vitejs.dev/"
+                                            favicon:
+                                                "https://vitejs.dev/logo.svg",
+                                            title: "Vite | Next Generation Frontend Tooling",
+                                            url: "https://vitejs.dev/",
                                         },
                                         {
-                                            "favicon": "https://www.rust-lang.org/static/images/favicon.svg",
-                                            "title": "Rust Programming Language",
-                                            "url": "https://www.rust-lang.org/"
+                                            favicon:
+                                                "https://www.rust-lang.org/static/images/favicon.svg",
+                                            title: "Rust Programming Language",
+                                            url: "https://www.rust-lang.org/",
                                         },
                                         {
-                                            "favicon": "https://www.sbert.net/_static/favicon.ico",
-                                            "title": "SentenceTransformers Documentation — Sentence Transformers documentation",
-                                            "url": "https://www.sbert.net/"
+                                            favicon:
+                                                "https://www.sbert.net/_static/favicon.ico",
+                                            title: "SentenceTransformers Documentation — Sentence Transformers documentation",
+                                            url: "https://www.sbert.net/",
                                         },
                                         {
-                                            "favicon": "https://github.githubassets.com/favicons/favicon-dark.svg",
-                                            "title": "GitHub Packages: Your packages, at home with their code · GitHub",
-                                            "url": "https://github.com/features/packages"
+                                            favicon:
+                                                "https://github.githubassets.com/favicons/favicon-dark.svg",
+                                            title: "GitHub Packages: Your packages, at home with their code · GitHub",
+                                            url: "https://github.com/features/packages",
                                         },
                                         {
-                                            "favicon": "https://www.youtube.com/s/desktop/d44eab58/img/favicon_32x32.png",
-                                            "title": "Meet Llama 3.1 - YouTube",
-                                            "url": "https://www.youtube.com/watch?v=kv1qGLlw9yg"
+                                            favicon:
+                                                "https://www.youtube.com/s/desktop/d44eab58/img/favicon_32x32.png",
+                                            title: "Meet Llama 3.1 - YouTube",
+                                            url: "https://www.youtube.com/watch?v=kv1qGLlw9yg",
                                         },
                                         {
-                                            "favicon": "https://docs.astral.sh/ruff/assets/ruff-favicon.png",
-                                            "title": "Ruff",
-                                            "url": "https://docs.astral.sh/ruff/"
+                                            favicon:
+                                                "https://docs.astral.sh/ruff/assets/ruff-favicon.png",
+                                            title: "Ruff",
+                                            url: "https://docs.astral.sh/ruff/",
                                         },
                                         {
-                                            "favicon": "https://biomejs.dev/img/favicon.svg",
-                                            "title": "Biome, toolchain of the web",
-                                            "url": "https://biomejs.dev/"
+                                            favicon:
+                                                "https://biomejs.dev/img/favicon.svg",
+                                            title: "Biome, toolchain of the web",
+                                            url: "https://biomejs.dev/",
                                         },
                                         {
-                                            "favicon": "https://posthog.com/favicon.svg?v=6e5ac8d4a5b381b5caa29396fbf7c955",
-                                            "title": "PostHog - How developers build successful products",
-                                            "url": "https://posthog.com/"
+                                            favicon:
+                                                "https://posthog.com/favicon.svg?v=6e5ac8d4a5b381b5caa29396fbf7c955",
+                                            title: "PostHog - How developers build successful products",
+                                            url: "https://posthog.com/",
                                         },
                                         {
-                                            "favicon": "https://www.solidjs.com/img/favicons/favicon-32x32.png",
-                                            "title": "SolidJS · Reactive Javascript Library",
-                                            "url": "https://www.solidjs.com/"
-                                        }
+                                            favicon:
+                                                "https://www.solidjs.com/img/favicons/favicon-32x32.png",
+                                            title: "SolidJS · Reactive Javascript Library",
+                                            url: "https://www.solidjs.com/",
+                                        },
                                     ]);
 
                                     const newTabGroup = new TabGroup();
@@ -148,12 +167,15 @@ function App() {
                                                 favicon,
                                                 title,
                                                 url,
-                                            })
+                                            }),
                                     );
 
                                     await addTabBundle([newTabGroup, newTabs]);
 
-                                    tabDBRefetch();
+                                    queryClient.invalidateQueries({
+                                        queryKey: ["tabgroups"],
+                                    });
+                                    tabDBRefetch(); // TODO: Replace this
                                     await switchToOrOpenTab(URLS.SAVED);
                                 },
                             ],
